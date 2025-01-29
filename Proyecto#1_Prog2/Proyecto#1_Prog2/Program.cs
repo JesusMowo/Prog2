a14e131b18df5a501e2f488958f46c2c09036e5e
JesusMowo <alejandrojesusmata@gmail.com>
2025-01-29 00:49:15 -0400

    First commit :3

---
+++ Proyecto#1_Prog2/Proyecto#1_Prog2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Proyect
+{
+    class ItemBiblioteca(string titulo, string autor, string categoria, string publicacion)
+    {
+        public String autor = autor;
+        public String titulo = titulo;
+        public String categoria = categoria;
+        public String publicacion = publicacion;
+
+        public virtual void MostrarInfo()
+        {
+            Console.WriteLine($"Titulo: {titulo}. autor: {autor}. categoria: {categoria}. fecha de publicacion: {publicacion}.");
+        }
+    }
+    class Libro(string titulo, string autor, string categoria, string publicacion) : ItemBiblioteca(titulo, autor, categoria, publicacion)
+    {
+        private readonly string TipoDeLectura = "Libro";
+
+        public override void MostrarInfo()
+        {
+            Console.Write($"Tipo De lectura: {TipoDeLectura}. ");
+            base.MostrarInfo();
+        }
+
+    }
+    class Revista(string titulo, string autor, string categoria, string publicacion) : ItemBiblioteca(titulo, autor, categoria, publicacion)
+    {
+        private readonly string TipoDeLectura = "Revista";
+
+        public override void MostrarInfo()
+        {
+            Console.Write($"Tipo De lectura: {TipoDeLectura}. ");
+            base.MostrarInfo();
+        }
+
+    }
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Libro libro = new ("50 Sombras de grey", "Yo", "Erotica", "1992");
+            libro.MostrarInfo();
+            Console.WriteLine("");
+            Revista revista = new ("Dou", "Bananirou", "Variedad", "2025");
+            revista.MostrarInfo();
+        }
+    }
+}
